5c4016694c1125511752501b5ee156520f45095c
Cristian H <chorta@ucsd.edu>
2023-09-05 21:19:44 -0700

    tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -13,6 +13,17 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-inter)"],
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'ucnavy': '#182B49',
+        'ucblue': '#00629B',
+        'ucyellow': '#FFCD00',
+        'ucgold': '#C69214',
+        'coolgray': '#747678',
+        'stone': '#B6B1A9',
+        'sand': '#F5F0E6',
+      },      
     },
   },
   plugins: [],
